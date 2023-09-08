192188bc8cdd4fa68a7bbdfd1e2284a1098ea4fe
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-08 13:52:28 -0300

    mudando para pastas especificas

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import "./Button.css";
-
-export default function Button(props) {
-    return (
-        <button type={props.type} className={props.className} title={props.title}>{props.children}</button>
-    );
-}
