a1380c689cf6fa37393c5bae922351256666337a
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-09-13 15:24:55 -0300

    test: add test unit componente SectionBoost

---
+++ src/components/SectionBoost/__tests__/index.test.js
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import SectionBoost from "..";
+
+it("renderiza componente SectionBoost sem erros", () => {
+  render(<SectionBoost />);
+});
